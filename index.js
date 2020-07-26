87e9b12e83a2e9d24082b4e17d5f8a52f578b662
Ryan Meinzer <ryanmeinzer@ryanmeinzer.com>
2020-07-25 21:52:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+
+let num1 = 31
+let num2 = 2
+let multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1
+
+let mod = 16 % 6
+
+let max = Math.max(1, 5, 10, 20)
